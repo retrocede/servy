b29e5e034a6e080f9756816b335565b2ee072519
Timothy Stapleton <retrocede@outlook.com>
2017-05-14 20:32:24 -0700

    Video 8: Path rewrites, error logging

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,13 +2,14 @@ defmodule Servy.Handler do
     def handle(request) do
         request
         |> parse
+        |> rewrite_path
         |> log
         |> route
+        |> track
         |> format_response
     end
 
-    def log(conv), do: IO.inspect conv
-
+    # Parse raw request into a map
     def parse(request) do
         # Parse the request string into the map
         [ method, path, _] =
@@ -19,6 +20,19 @@ defmodule Servy.Handler do
 
         %{ method: method, path: path, resp_body: "", status: nil}
     end
+
+    # Generic logger
+    def log(conv), do: IO.inspect conv
+
+    #
+    # Path Rewrites
+    #
+    def rewrite_path(%{ path: "/wildlife" } = conv) do
+        %{ conv | path: "/wildthings" }
+    end
+
+    # catch all, rewrite not needed
+    def rewrite_path(conv), do: conv
 
     #
     # Routes
@@ -39,10 +53,19 @@ defmodule Servy.Handler do
         %{ conv | status: 403, resp_body: "Deleting a bear is forbidden!" }
     end
     # catch all route
-    def route(%{ method: _method, path: path } = conv) do
+    def route(%{ path: path } = conv) do
         %{ conv | status: 404, resp_body: "No #{ path } here!"}
     end
 
+    # Track: Log error routes
+    def track(%{ status: 404, path: path } = conv) do
+        IO.puts "Warning #{ path } is on the loose!"
+        conv
+    end
+    # catch all, tracking not needed
+    def track(conv), do: conv
+
+    # Format HTTP Resposne
     def format_response(conv) do
         # Use the values in the map to create an HTTP response string:
         """
@@ -53,7 +76,7 @@ defmodule Servy.Handler do
         #{ conv.resp_body }
         """
     end
-
+    # helper function to map status codes to their message
     defp status_reason(code) do
         %{
             200 => "OK",
@@ -126,6 +149,19 @@ IO.puts response
 # Sample req 5
 request = """
 DELETE /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle request
+
+IO.puts response
+
+# Sample req 6
+request = """
+GET /wildlife HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
