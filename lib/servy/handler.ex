f8c1bcfe643eea8412c437034f3fa8a9aac4b08c
Timothy Stapleton <retrocede@outlook.com>
2017-05-14 19:32:00 -0700

    Video 6: handling additional git reqs

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,9 +2,12 @@ defmodule Servy.Handler do
     def handle(request) do
         request
         |> parse
+        |> log
         |> route
         |> format_response
     end
+
+    def log(conv), do: IO.inspect conv
 
     def parse(request) do
         # Parse the request string into the map
@@ -17,10 +20,8 @@ defmodule Servy.Handler do
         %{ method: method, path: path, resp_body: "" }
     end
 
-    def route(conv) do
-        # Create a new map that also has the response body:
-        %{ conv | resp_body: "Bears, Lions, Tigers" }
-    end
+    def route(%{ method: "GET", path: "/wildthings" } = conv), do: %{ conv | resp_body: "Bears, Lions, Tigers"}
+    def route(%{ method: "GET", path: "/bears" } = conv), do: %{ conv | resp_body: "Teddy, Smokey, Paddington"}
 
     def format_response(conv) do
         # Use the values in the map to create an HTTP response string:
@@ -34,8 +35,35 @@ defmodule Servy.Handler do
     end
 end
 
+# Sample req 1
 request = """
 GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle request
+
+IO.puts response
+
+# Sample req 2
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle request
+
+IO.puts response
+
+# Sample req 3
+request = """
+GET /bigfoot HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
