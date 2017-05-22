bd8321ee5076d61476dc6ee4636aad5ed04660bb
Timothy Stapleton <retrocede@outlook.com>
2017-05-21 22:10:18 -0700

    more video 8 exercises (regex and logger)

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,4 +1,6 @@
 defmodule Servy.Handler do
+    require Logger
+
     def handle(request) do
         request
         |> parse
@@ -23,8 +25,10 @@ defmodule Servy.Handler do
     end
 
     # Generic logger
-    def log(conv), do: IO.inspect conv
-
+    def log(conv) do
+        Logger.info inspect conv
+        conv
+    end
     #
     # Path Rewrites
     #
@@ -32,12 +36,21 @@ defmodule Servy.Handler do
         %{ conv | path: "/wildthings" }
     end
 
-    def rewrite_path(%{ path: "/bears?id=" <> id} = conv) do
-        %{ conv | path: "/bears/#{id}"}
+    def rewrite_path(%{ path: path } = conv) do
+        regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
+        captures = Regex.named_captures(regex, path)
+        rewrite_path_captures(conv, captures)
     end
-
     # catch all, rewrite not needed
     def rewrite_path(conv), do: conv
+
+    # helper to rewrite thing?id=# routes
+    def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
+        %{ conv | path: "/#{ thing }/#{ id }" }
+    end
+
+    def rewrite_path_captures(conv, nil), do: conv
+
 
     #
     # Routes
@@ -74,7 +87,7 @@ defmodule Servy.Handler do
 
     # Track: Log error routes
     def track(%{ status: 404, path: path } = conv) do
-        IO.puts "Warning #{ path } is on the loose!"
+        Logger.error "Error 404: #{ path } not found."
         conv
     end
     # catch all, tracking not needed
