0fbb0cdea9f796c28c74cdc9be27c45dc3a80da6
Timothy Stapleton <retrocede@outlook.com>
2017-06-26 21:39:45 -0700

    Video 15 changes

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -3,6 +3,7 @@ defmodule Servy.Handler do
   @moduledoc "Handles HTTP requests."
 
   alias Servy.Conv
+  alias Servy.BearController
 
   @pages_path Path.expand("../../pages", __DIR__)
 
@@ -16,7 +17,6 @@ defmodule Servy.Handler do
     |> rewrite_path
     |> log
     |> route
-    |> emojify
     |> track
     |> format_response
   end
@@ -31,7 +31,7 @@ defmodule Servy.Handler do
   end
   # bears
   def route(%Conv{ method: "GET", path: "/bears" } = conv) do
-    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
+    BearController.index(conv)
   end
   def route(%Conv{ method: "GET", path: "/bears/new" } = conv) do
     "../../pages"
@@ -41,14 +41,14 @@ defmodule Servy.Handler do
     |> handle_file(conv)
   end
   def route(%Conv{ method: "GET", path: "/bears/" <> id } = conv) do
-    %{ conv | status: 200, resp_body: "Bear #{ id }" }
+    params = Map.put(conv.params, "id", id)
+    BearController.show(conv, params)
   end
   def route(%Conv{ method: "DELETE", path: "/bears/" <> _id } = conv) do
     %{ conv | status: 403, resp_body: "Deleting a bear is forbidden!" }
   end
-  # name=Baloo&type=Brown
   def route(%Conv{ method: "POST", path: "/bears"} = conv) do
-    %{ conv | status: 201, resp_body: "Created a #{conv.params["type"]} bear named #{conv.params["name"]}."}
+    BearController.create(conv, conv.params)
   end
 
   def route(%Conv{ method: "GET", path: "/about" } = conv) do
@@ -81,17 +81,6 @@ defmodule Servy.Handler do
   def handle_file({ :error, reason }, conv) do
     %{ conv | status: 500, resp_body: "File error: #{ reason }"}
   end
-
-  # Emojify: Add emojis to worthy responses
-  def emojify(%Conv{ status: 200 } = conv) do
-    emoji_bod = String.duplicate(" ☜(⌒▽⌒)☞ ", 3) <> "\n"
-    <> conv.resp_body <> "\n"
-    <> String.duplicate(" (｡◕‿◕｡) ", 3)
-    %{ conv | resp_body: emoji_bod}
-  end
-  # catch all, not worthy of emoji splendor
-  def emojify(%Conv{} = conv), do: conv
-
 
   # Format HTTP Response
   def format_response(%Conv{} = conv) do
